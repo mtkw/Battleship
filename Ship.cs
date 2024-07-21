d4c5cfc970dbd0a6cf91a32f7dd3110df82246af
Mateusz Kawęczyński <mati.kaweczynski@gmail.com>
2024-07-21 14:50:44 +0200

    Dodaj pliki projektów.

---
+++ Ship.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    internal class Ship
+    {
+        private ShipTypeEnum shipType { get; }
+        private int lenght { get; }
+        private List<Square>? Squares { get; set; }
+        private List<Square> hits = new();
+        private bool isHitted = false;
+        private bool isSunk = false;
+
+        public Ship(ShipTypeEnum shipType, List<Square> squares)
+        {
+            this.shipType = shipType;
+            this.lenght = (int)shipType;
+            Squares = new (lenght);
+        }
+    }
+}
---
+++ ShipTypeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    enum ShipTypeEnum
+    {
+        Carrier = 1,
+        Cruiser = 2,
+        Battleship = 3,
+        Submarine = 4,
+        Destroyer = 4
+    }
+}
